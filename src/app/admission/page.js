bc0ea89fbbaa7949f10c3fb51e7e6fb8dd0da952
abdurrashidkhan <abdurrashidkhan.destop@gmail.com>
2024-07-11 10:41:28 +0600

    proteted admission route / can't visit without login

--- src/app/admission/page.js
+++ src/app/admission/page.js
@@ -1,18 +1,41 @@
+"use client"
+import CheckAdmin from "@/Components/Admin/CheckAdmin";
+import CheckingUser from "@/Components/Admin/checkingUser";
 import AdmissionForm from "@/Components/Admission/Admission";
+import { useEffect, useState } from "react";
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
+import { auth } from "../firebase.init";
+import Loading from "../loading";
 import "./style.css";
 
 export default function Admission() {
-  // const [isLoading, seIsLoading] = useState(false);
-//   const checkingUsers = CheckingUser(); // call checking user fund or not
-//   useEffect(() => {
-//     CheckAdmin(user, signOut);
-//   }, [user, signOut]);
+  const [user, loading, error] = useAuthState(auth);
+  const [signOut, outLoading, OutError] = useSignOut(auth);
+  const [isLoading, seIsLoading] = useState(false);
+  const checkingUsers = CheckingUser(); // call checking user fund or not
+  useEffect(() => {
+    CheckAdmin(user, signOut);
+  }, [user, signOut]);
 // // 
 
 
-// useEffect(() => {
-//   CheckAdmin(user, signOut);
-// }, [user, signOut]);
+useEffect(() => {
+  CheckAdmin(user, signOut);
+}, [user, signOut]);
+if (loading || outLoading) {
+  return <Loading></Loading>;
+}
+if (error || OutError) {
+  console.log(error?.message);
+}
+
+// 
+  if (loading || outLoading) {
+    return <Loading></Loading>;
+  }
+  if (error || OutError) {
+    console.log(error?.message);
+  }
   return (
     <section className="h-auto dark:bg-[#122033] dark:text-[#dddddd] bg-[#eeeeee] text-[#000] ease-in-out duration-1000">
       <div className="container mx-auto py-5">
