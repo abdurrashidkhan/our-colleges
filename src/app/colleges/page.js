2129b9ba093696a49407e1480d8ec348285246e9
abdurrashidkhan <abdurrashidkhan.destop@gmail.com>
2024-07-14 15:02:23 +0600

    error

--- src/app/colleges/page.js
+++ src/app/colleges/page.js
@@ -2,7 +2,7 @@ import Colleges from "@/Components/Colleges/Colleges";
 
 export default function CollegesPage() {
   return (
-    <section className="h-[100vh] dark:bg-[#122033] dark:text-[#dddddd] bg-[#eeeeee] text-[#000] ease-in-out duration-1000">
+    <section className="h-auto dark:bg-[#122033] dark:text-[#dddddd] bg-[#eeeeee] text-[#000] ease-in-out duration-1000">
       <div className="pt-20">
         <Colleges />
       </div>
